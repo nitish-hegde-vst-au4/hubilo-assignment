0175ebdebc78155bfed5a25b8ada522d3c5127dc
Nitish Hegde <nitishh21@yahoo.com>
2021-04-21 23:07:34 +0530

    Revert "temp commit"
    This reverts commit 8477e91b5a371ab4bfce81e24b0433fbe821fef4.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { Fragment } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Route, Switch } from 'react-router-dom'
-import FormPage from './components/FormPage/FormPage'
-import ListPage from './components/ListPage/ListPage'
-import Navbar from './components/Navbar/Navbar'
-import Modal from './components/Modal/Modal'
-import Backdrop from './components/Backdrop/Backdrop'
-import FormContainer from './components/FormContainer/FormContainer'
-import { updateUser, editUser } from './redux/actions'
+import React, { Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import FormPage from './components/FormPage/FormPage';
+import ListPage from './components/ListPage/ListPage';
+import Navbar from './components/Navbar/Navbar';
+import Modal from './components/Modal/Modal';
+import Backdrop from './components/Backdrop/Backdrop';
+import FormContainer from './components/FormContainer/FormContainer';
+import { updateUser, editUser } from './redux/actions';
 
 function App() {
-  const userToEdit = useSelector((state) => state.userToEdit)
-  const dispatch = useDispatch()
+  const userToEdit = useSelector((state) => state.userToEdit);
+  const dispatch = useDispatch();
   return (
     <Fragment>
       <Navbar />
       {userToEdit ? (
-        <div className="modal-backdrop-container">
-          <Modal user={userToEdit} action={updateUser} component={FormContainer} />
+        <div className='modal-backdrop-container'>
+          <Modal
+            user={userToEdit}
+            action={updateUser}
+            component={FormContainer}
+          />
           <Backdrop onClick={() => dispatch(editUser(null))} />
         </div>
       ) : null}
 
       <Switch>
-        <Route exact path="/" component={FormPage} />
-        <Route path="/users-list" component={ListPage} />
+        <Route exact path='/' component={FormPage} />
+        <Route path='/users-list' component={ListPage} />
       </Switch>
     </Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
